ffcd492958f879286cb0d7eba23f0de454c9fbce
zbjbaggio@163.com <zbjbaggio@163.com>
2017-10-04 21:31:15 +0800

    包名改成小写

--- src/main/java/com/springboot/base/JsonSerializer/DateForOrderSerializer.java
+++ src/main/java/com/springboot/base/JsonSerializer/DateForOrderSerializer.java
@@ -1,4 +1,4 @@
-package com.springboot.base.JsonSerializer;
+package com.springboot.base.jsonserializer;
 
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
