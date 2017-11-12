24e305d59d2fad4abce09af982f63b16a6452b2d
zbjbaggio@163.com <zbjbaggio@163.com>
2017-11-12 17:08:30 +0800

    增加email工具类

--- src/main/java/com/springboot/base/data/vo/OrderVO.java
+++ src/main/java/com/springboot/base/data/vo/OrderVO.java
@@ -1,8 +1,11 @@
 package com.springboot.base.data.vo;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.springboot.base.JsonSerializer.CustomDoubleSerialize;
 import lombok.Data;
 
+import java.math.BigDecimal;
 import java.util.Date;
 
 @Data
@@ -11,5 +14,28 @@ public class OrderVO {
     private Long id;
 
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
-    private Date createTime;
+    private Date create_time;
+
+    private String order_no;
+
+    private String email;
+
+    private String receiver_name;
+
+    private String receiver_address;
+
+    private String receiver_city;
+
+    private String receiver_country;
+
+    private String remark;
+
+    private String description;
+
+    private String postage;
+
+    private byte status;
+
+    @JsonSerialize(using = CustomDoubleSerialize.class)
+    private BigDecimal amount;
 }
