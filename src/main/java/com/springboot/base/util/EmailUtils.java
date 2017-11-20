e12007c50959a13cb38f94faf6cdfa9ad4f0eb22
zhangbaojun <jay@4zlink.com>
2017-11-20 13:31:54 +0800

    管理系统订单信息修改

--- src/main/java/com/springboot/base/util/EmailUtils.java
+++ src/main/java/com/springboot/base/util/EmailUtils.java
@@ -28,15 +28,18 @@ public class EmailUtils {
     // 发件人邮箱的 SMTP 服务器地址, 必须准确, 不同邮件服务器地址不同, 一般(只是一般, 绝非绝对)格式为: smtp.xxx.com
     private static String myEmailSMTPHost;
 
+    private static String myEmailSMTPPort;
+
     public static void sendEmail(EmailDTO emailDTO) throws Exception {
         // 1. 创建参数配置, 用于连接邮件服务器的参数配置
         Properties props = new Properties();                    // 参数配置
         props.setProperty("mail.transport.protocol", "smtp");   // 使用的协议（JavaMail规范要求）
         props.setProperty("mail.smtp.host", myEmailSMTPHost);   // 发件人的邮箱的 SMTP 服务器地址
-        props.setProperty("mail.smtp.auth", "true");            // 需要请求认证
+        props.setProperty("mail.smtp.port", myEmailSMTPPort);
+        props.setProperty("mail.smtp.auth", "true");
+        props.setProperty("mail.smtp.starttls.enable", "true");
         // PS: 某些邮箱服务器要求 SMTP 连接需要使用 SSL 安全认证 (为了提高安全性, 邮箱支持SSL连接, 也可以自己开启),
         //     如果无法连接邮件服务器, 仔细查看控制台打印的 log, 如果有有类似 “连接失败, 要求 SSL 安全连接” 等错误,
-        //     打开下面 /* ... */ 之间的注释代码, 开启 SSL 安全连接。
         /*
         // SMTP 服务器的端口 (非 SSL 连接的端口一般默认为 25, 可以不添加, 如果开启了 SSL 连接,
         //                  需要改为对应邮箱的 SMTP 服务器的端口, 具体可查看对应邮箱服务的帮助,
@@ -47,6 +50,7 @@ public class EmailUtils {
         props.setProperty("mail.smtp.socketFactory.fallback", "false");
         props.setProperty("mail.smtp.socketFactory.port", smtpPort);
         */
+        //     打开下面 /* ... */ 之间的注释代码, 开启 SSL 安全连接。
         // 2. 根据配置创建会话对象, 用于和邮件服务器交互
         Session session = Session.getInstance(props);
         session.setDebug(true);                                 // 设置为debug模式, 可以查看详细的发送 log
@@ -115,8 +119,13 @@ public class EmailUtils {
         EmailUtils.myEmailPassword = myEmailPassword;
     }
 
-    @Value("${email.smtp}")
+    @Value("${email.smtpHost}")
     public void setMyEmailSMTPHost(String myEmailSMTPHost) {
         EmailUtils.myEmailSMTPHost = myEmailSMTPHost;
+    }
+
+    @Value("${email.smtpPort}")
+    public void setMyEmailSMTPPort(String myEmailSMTPPort) {
+        EmailUtils.myEmailSMTPPort = myEmailSMTPPort;
     }
 }
