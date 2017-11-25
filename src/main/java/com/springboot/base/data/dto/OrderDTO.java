d0c38bea21aeab63d84aed95dfef6574e72893ac
zhangbaojun <jay@4zlink.com>
2017-11-25 15:08:43 +0800

    订单增加地址2

--- src/main/java/com/springboot/base/data/dto/OrderDTO.java
+++ src/main/java/com/springboot/base/data/dto/OrderDTO.java
@@ -7,7 +7,6 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import javax.validation.Valid;
-import javax.validation.constraints.Max;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.List;
