1a2ec9625ee16f0dc3b24b42fee7ce86a155833e
zhangbaojun <jay@4zlink.com>
2018-01-23 11:25:36 +0800

    加入权限

---
+++ src/main/java/com/springboot/base/util/IpVerifyUtil.java
@@ -0,0 +1,34 @@
+package com.springboot.base.util;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ *
+ * Created by lilong on 16-6-14.
+ */
+public class IpVerifyUtil {
+    // ip地址
+    public static String getIp(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("http_client_ip");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        // 如果是多级代理，那么取第一个ip为客户ip
+        if (ip != null && ip.indexOf(",") != -1) {
+            ip = ip.substring(0, ip.indexOf(",")).trim();
+        }
+        return ip;
+    }
+}
